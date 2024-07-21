626c17d1d5e1932ea3e1a9a896dfe47940045ad1
HDL951236874 <2862308357@qq.com>
2024-07-21 12:42:26 -0400

    Reworked the DTO class

--- src/test/java/com/edaolin/blog/RandomAdder.java
+++ src/test/java/com/edaolin/blog/RandomAdder.java
@@ -1,10 +1,16 @@
 package com.edaolin.blog;
 
+import com.edaolin.blog.Data.Blog;
+import com.edaolin.blog.Data.Role;
+import com.edaolin.blog.Data.User;
 import com.edaolin.blog.Service.BlogService;
 import com.edaolin.blog.Service.UserService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class RandomAdder {
@@ -17,4 +23,10 @@ public class RandomAdder {
     @Autowired
     UserService userService;
 
+    @Test
+    public void AddingUsers(){
+        User user1 = User.builder().name("Name1").password("Password1").blogs(new ArrayList<Blog>()).role(new Role()).build();
+        User user2 = User.builder().name("Name2").password("Password2").blogs(new ArrayList<Blog>()).role(new Role()).build();
+        userService.saveUser(user1);
+    }
 }
